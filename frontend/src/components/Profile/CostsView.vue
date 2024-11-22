418df616fd0548d10f2594c8bdb512ba785270c0
Bellou1337 <artm_ivan05@mail.ru>
2024-11-22 15:59:24 +0300

    Add Profile components

---
+++ frontend/src/components/Profile/CostsView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <!--Costs-->
+        <div>
+
+        </div>
+
+        <!--Income-->
+        <div>
+            
+        </div>
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        
+    }
+    )   
+</script>
---
+++ frontend/src/components/ProfileView.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <UserInfo
+        :email = userEmail
+        :name = userName>
+        </UserInfo>
+    </header>
+    
+    <main>
+
+    </main>
+    
+    <footer>
+
+    </footer>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+import UserInfo from '@/components/Profile/UserInfoView.vue';
+import Costs from '@/components/Profile/CostsView.vue';
+
+const userName = ref('Username');
+const userEmail = ref('asdasd@mail.ru');
+
+
+defineProps(
+
+)
+</script>
