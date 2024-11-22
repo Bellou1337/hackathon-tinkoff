4067f8a58c5e845b0c9b6c9817a096649ce57d1e
Bellou1337 <artm_ivan05@mail.ru>
2024-11-22 22:42:02 +0300

    cors

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Depends
-
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import wallet
 
 from .routers import auth_router
@@ -8,6 +8,22 @@ from .routers import auth_router
 app = FastAPI(
     title = "Hackathon API",    
 )
+
+origins = [
+"http://localhost:3000",
+"http://127.0.0.1:3000",
+"http://git-ts2.ru:8000",
+"*",
+]
+
+app.add_middleware(
+CORSMiddleware,
+allow_origins=origins, 
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"],
+)
+
 
 app.include_router(
     auth_router.router,
