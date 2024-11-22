50171c489fc153ccbe1f3268efa4cd29bb38228e
Ruban Daniil <danya.ruban.2005@mail.ru>
2024-11-22 15:52:54 +0300

    Profile files

---
+++ frontend/src/components/ProfileView.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <UserInfo
+        :email = userEmail
+        :name = userName>
+        </UserInfo>
+    </header>
+    
+    <main>
+
+    </main>
+    
+    <footer>
+
+    </footer>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+import UserInfo from '@/components/Profile/UserInfoView.vue';
+
+const userName = ref('Username');
+const userEmail = ref('asdasd@mail.ru');
+
+
+defineProps(
+
+)
+</script>
