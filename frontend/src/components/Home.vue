04f26ff6468b40639e4f069c4f088761db7d4ae3
myawur <mrlinesea420@gmail.com>
2024-11-22 16:22:43 +0300

    фикс импортов

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,9 +1,9 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue'
-import Profile from '@/components/profile/ProfileView.vue'
+import ProfileView from '@/components/profile/ProfileView.vue'
 </script>
 
 <template>
   <Navbar />
-  <Profile />
+  <ProfileView />
 </template>
---
+++ frontend/src/components/Profile/ProfileView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import UserInfoView from '@/components/profile/information/UserInfoView.vue'
+import MoneyView from '@/components/profile/information/MoneyView.vue'
+import WalletsView from '@/components/profile/information/wallets/WalletsView.vue'
+</script>
+
+<template>
+  <UserInfoView />
+  <MoneyView />
+  <WalletsView />
+</template>
